c26e024ef5e345c920b6025a528f4eb1a1746c99
Kazuma Takata <neilbuzztakata@gmail.com>
2020-08-19 17:58:10 +0900

    init

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def login(request):
+    return render(request, "login.html") 
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def pipeline(*args, **kwargs): 
+    breakpoint()
+    pass
